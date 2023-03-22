ec5ce479838f9210f286f615748a7d59dbb0240d
davidkern13 <david33belle@gmail.com>
2023-03-22 21:29:16 +0200

    fix: example component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Example from './Example';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+    <Example />
 );
